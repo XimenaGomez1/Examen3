24a1ea327da46d2412338a6b416079aedab9354b
Ximena <Ximena_Gomez@cramerpuno.edu.com>
2021-12-13 12:59:00 -0500

    first commit

--- script.js
+++ script.js
@@ -1,34 +1,25 @@
-const sounds=[
-    'CLAP', 
-    'HIHAT', 
-    'KICK', 
-    'OPENHAT', 
-    'BOOM', 
-    'RIDE', 
-    'SNARE', 
-    'TOM', 
-    'TINK'
-];
+let list=document.getElementById("form");
+let conten=document.getElementById("conten");
+let figur= [];
 
-sounds.forEach((sound) => {
-
-    const btn = document.createElement('button');
-    btn.classList.add('btn');
-    btn.innerText = sound;
-    btn. addEventListener('click', () => {
-        stopS();
-        document.getElementById(sound).play();
-    })
-
-    document.getElementById('Cuadritos').appendChild(btn);
-    
-});
-function stopS(){
-
-    sounds.forEach((sound) => 
-    {
-        const song = document.getElementById(sound);
-        song.pause();
-        song.currentTime=0; 
+list.addEventListener("submit", (event)=>{
+    event.preventDefault();
+    let data =new FormData(list);
+    figur.push({
+        texto: data.get("nombre"),
+        color: data.get("color"),
+        figura: data.get("figura"),
+        
     });
-}
+    pintar_figuras();
+});
+const pintar_figuras=()=>
+{
+    content.innerHTML="";
+    figur.forEach((e)=>{
+        content.insertAdjacentHTML (
+            "beforeend",
+            '<div class="${e.figura}" style="background:$(e.color)"> $(e.nombre)<div/>'
+        );
+    });
+};
